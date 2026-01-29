b72976db83998ae8cc6e0da5627b280b69c8e42f
adihora7 <aadityadihora7@gmail.com>
2026-01-29 13:40:54 -0500

    Implement Hero Scroll Animation

--- Cocktails/src/App.jsx
+++ Cocktails/src/App.jsx
@@ -9,9 +9,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
  return (
 	<main>
-	 <Navbar />
-	 <Hero />
-	 <div className="h-dvh bg-black" />
+		<Navbar />
+		<Hero />
 	</main>
  )
 }
